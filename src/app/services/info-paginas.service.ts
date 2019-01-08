2aa993721762387ae348f89828a8a769c3174294
Misael Calisaya <misael.calisaya@gmail.com>
2019-01-08 17:20:56 -0300

    listo para produccion

---
+++ src/app/services/info-paginas.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+  info: InfoPagina = {};
+  cargada = false;
+
+  equipo: any[] = [];
+
+  constructor( private http: HttpClient) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+}
+
+private cargarInfo() {
+
+  //console.log('Servicio de infoPagina listo');
+// leer el archivo JSON
+  this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+  }
+
+private cargarEquipo() {
+
+  this.http.get('https://angular-html-58fa8.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+
+        this.equipo = resp;
+        //console.log(resp);
+      });
+
+  }
+}
